1e74b601cfb2a15eafb7bd336e6b5405f28c74f8
Diego Mazzoni <diego.mazzoni@estudante.ufjf.br>
2023-12-13 20:36:55 -0300

    adiciona funcao tansacao

--- estoque.js
+++ estoque.js
@@ -3,15 +3,35 @@ let estoque = {
  'joao': [   
     {'tipo': "maca", 'qtd': 1},
     {'tipo': "pera", 'qtd': 1},
-];
+],
  'maria': [ 
     {'tipo': "maca", 'qtd': 2},
-    {'tipo': "pera", 'qtd': 4},
- ];
+    {'tipo': "banana", 'qtd': 4},
+ ]
 };
 
 export function getEstoque(){
     return structuredClone(estoque);
 }
+
+export function transacao(origem, destino, quantidade, fruta){
+    if(origem === 'pomar'){
+        const pessoa = estoque[destino];
+        let monte;
+        for(let i = 0; i < pessoa.length; i++){
+            if(pessoa[i].tipo === fruta){
+                monte = pessoa[i];
+                break;
+            }
+        }
+
+        if(monte === undefined){
+            monte = {'tipo': fruta, 'qtd': 0};
+            pessoa.push(monte);
+        }
+        monte.qtd += quantidade;
+        return;
+        }
+    }
 
 // export {getEstoque}
