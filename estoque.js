c14d4209293136ec5359c28304fdaf0497df7430
Diego Mazzoni <diego.estudante@ufjf.br>
2023-12-17 20:42:39 -0300

    cria função de passar frutas para maria e joao

--- estoque.js
+++ estoque.js
@@ -24,8 +24,23 @@ export function transacao(origem, destino, quantidade, fruta){
     }
         
     if(destino === "pomar"){
-            dePessoaParaPomar(origem, quantidade, fruta);   
+        dePessoaParaPomar(origem, quantidade, fruta);   
     }
+    if(origem === 'joao'){
+        deJoaoParaPessoa(destino, quantidade, fruta);
+    }
+        
+    if(destino === "joao"){
+        dePessoaParaJoao(origem, quantidade, fruta);   
+    }
+    if(origem === 'maria'){
+        deMariaParaPessoa(destino, quantidade, fruta);
+    }
+        
+    if(destino === "maria"){
+        dePessoaParaMaria(origem, quantidade, fruta);   
+    }
+    
 }
 
 function dePessoaParaPomar(origem, quantidade, fruta){
@@ -59,5 +74,72 @@ function dePomarParaPessoa(destino, quantidade, fruta){
         }
         monte.qtd += quantidade;
         return;
-    }
+}
+
+function dePessoaParaJoao(origem, quantidade, fruta){
+    const pessoa = estoque[origem];
+            let monte;
+            for(let i = 0; i < pessoa.length; i++){
+                if(pessoa[i].tipo === fruta){
+                    monte = pessoa[i];
+                    break;
+                }
+            }    
+        if(!monte){
+            return;
+        }
+        monte.qtd -= Math.min(quantidade, monte.qtd); 
+
+}
+
+function deJoaoParaPessoa(destino, quantidade, fruta){
+    const pessoa = estoque[destino];
+        let monte;
+        for(let i = 0; i < pessoa.length; i++){
+            if(pessoa[i].tipo === fruta){
+                monte = pessoa[i];
+                break;
+            }
+        }
+        if(!monte){
+            monte = {'tipo': fruta, 'qtd': 0};
+            pessoa.push(monte);
+        }
+        monte.qtd += quantidade;
+        return;
+}
+
+function dePessoaParaMaria(origem, quantidade, fruta){
+    const pessoa = estoque[origem];
+            let monte;
+            for(let i = 0; i < pessoa.length; i++){
+                if(pessoa[i].tipo === fruta){
+                    monte = pessoa[i];
+                    break;
+                }
+            }    
+        if(!monte){
+            return;
+        }
+        monte.qtd -= Math.min(quantidade, monte.qtd); 
+
+}
+
+function deMariaParaPessoa(destino, quantidade, fruta){
+    const pessoa = estoque[destino];
+        let monte;
+        for(let i = 0; i < pessoa.length; i++){
+            if(pessoa[i].tipo === fruta){
+                monte = pessoa[i];
+                break;
+            }
+        }
+        if(!monte){
+            monte = {'tipo': fruta, 'qtd': 0};
+            pessoa.push(monte);
+        }
+        monte.qtd += quantidade;
+        return;
+}
+
 // export {getEstoque}
